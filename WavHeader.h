a2316524b1af831da5d2b8472b237b6bed1f12b1
jacobghunter <98200224+jacobghunter@users.noreply.github.com>
2022-04-12 21:23:54 -0700

    trying to read .wav
    messed around with structs and reading a .wav file

---
+++ WavHeader.h
@@ -0,0 +1,23 @@
+struct int_holder {
+    int num;
+    int num2;
+};
+
+
+struct wav_header {
+    char chunk_ID[4];
+    int chunk_size;
+    char format[4];
+
+    int subchunk1_size;
+    short audio_format;
+    short num_channels;
+    int sample_rate;
+    int byte_rate;
+    short block_align;
+    short bits_per_sample;
+
+    char data_header[4];
+    int data_bytes;
+};
+
--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-
+#include "WavHeader.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
 
 //read all data from file into an array of size determined by the file size, maybe 2d array for stereo audio
 
@@ -15,3 +19,28 @@
 //echo: apply a delay effect with decreasing magnitude to each section of audio
 
 //low pass filter: tune down all audio above a certain threshold to be below that threshhold, seems like reverse normalization
+
+
+
+int main() {
+    // int list [2];
+    // list[0] = 1;
+    // list[1] = 2;
+    // int_holder number;
+    // number = (int_holder){.num = 1, .num2 = 2};
+    // std::cout << number.num << std::endl;
+
+    std::ifstream file("untitled.wav", std::ios::binary | std::ios::in);
+    // short* buffer = nullptr;
+
+    // char* buffer2 = nullptr;
+    // file.read(buffer2, 1);
+    // std::cout << buffer2 << std::endl;
+
+    std::vector<unsigned char> vect_buffer(std::istreambuf_iterator<char>(file), {});
+    for (int x = 0; x < 40; x++) {
+        std::cout << vect_buffer[x];
+    }
+    std::cout << std::endl;
+    return 0;
+}
